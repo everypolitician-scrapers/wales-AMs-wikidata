fced2b8c0480fa850d394bd87e3ff1024da90ffc
Tony Bowden <tony@tmtm.com>
2016-02-24 07:10:05 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'rest-client'
 
-@pages = [
-  'Category:Wales AMs 2011–',
-]
 
-@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each_with_index do |id, i|
-  data = WikiData::Fetcher.new(id: id).data('en') or next
-  # puts "%s %s" % [data[:id], data[:name]]
-  ScraperWiki.save_sqlite([:id], data)
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = WikiData::Category.new( 'Category:Wales AMs 2011–', 'en').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
