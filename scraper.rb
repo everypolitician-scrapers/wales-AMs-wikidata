ae47d49a92eb9bf61568e90214bcd0b35c3952cc
Tony Bowden <tony@tmtm.com>
2016-06-07 13:32:14 +0100

    Add members of 5th term

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,6 @@
 require 'wikidata/fetcher'
 
 
-names = WikiData::Category.new( 'Category:Wales AMs 2011–16', 'en').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+names_4 = WikiData::Category.new( 'Category:Wales AMs 2011–16', 'en').member_titles
+names_5 = WikiData::Category.new( 'Category:Wales AMs 2016–21', 'en').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_4 | names_5 })
