a55c4908fd3f0be46646a2324e5a64de2880e9db
Tony Bowden <tony@tmtm.com>
2016-06-19 11:00:16 +0100

    Also use Welsh Wikipedia

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,6 @@ require 'wikidata/fetcher'
 
 names_4 = WikiData::Category.new( 'Category:Wales AMs 2011–16', 'en').member_titles
 names_5 = WikiData::Category.new( 'Category:Wales AMs 2016–21', 'en').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_4 | names_5 })
+
+names_cy = WikiData::Category.new( 'Categori:Aelodau Cynulliad Cenedlaethol Cymru 2016–2021', 'cy').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_4 | names_5, cy: names_cy })
