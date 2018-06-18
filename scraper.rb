ec76c362e8d1e7d25b61640b07ac407299e9686e
Tony Bowden <tony@tmtm.com>
2018-06-18 15:06:42 +0100

    Fetch all members via Wikidata

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'scraperwiki'
 require 'wikidata/fetcher'
 
+query = 'SELECT DISTINCT ?item { ?item wdt:P39/wdt:P279* wd:Q3406079 }'
+ids = EveryPolitician::Wikidata.sparql(query)
 
-names_4 = WikiData::Category.new( 'Category:Wales AMs 2011–16', 'en').member_titles
-names_5 = WikiData::Category.new( 'Category:Wales AMs 2016–21', 'en').member_titles
-
-names_cy = WikiData::Category.new( 'Categori:Aelodau Cynulliad Cenedlaethol Cymru 2016–2021', 'cy').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_4 | names_5, cy: names_cy })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
